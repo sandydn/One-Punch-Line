7a83bd151013ac3b43b02e652d606b0610938afe
Melroune <kerkeb@gmail.com>
2019-10-26 06:46:16 +0200

    add logique question

--- client/src/componants/Text.jsx
+++ client/src/componants/Text.jsx
@@ -1,33 +1,53 @@
-import React, {Component} from 'react'
+import React, { Component } from "react"
 // import Start from '../data/Start.json'
 
 class Text extends Component {
-    state={data: this.props.data,
-            page: this.props.data[0],
-            isload: false
-        }
+  state = {
+    data: this.props.data,
+    page: this.props.data[0],
+    isLoad: true
+  }
 
-    setPage = async(id) => {
-        const poulet = await this.props.data.filter((one)=> one.id.includes(id))
-        console.log(poulet)
-        await this.setState({page: poulet})
-    }
+  // setPage = async(id) => {
+  //     const poulet = await this.props.data.filter((one)=> one.id.includes(id))
+  //     console.log(poulet)
+  //     await this.setState({page: poulet})
+  // }
 
-    renderButton = () =>{
-        const map = !!this.state.nextId ? console.log('REDIRECT') : this.state.page.questions.map((one) => <button onClick={() => this.setPage(one.nextId)}>{one.value}</button>)
-        return map
-    }
+  // renderButton = () =>{
+  //     const map = !!this.state.nextId ? console.log('REDIRECT') : this.state.page.questions.map((one) => <button onClick={() => this.setPage(one.nextId)}>{one.value}</button>)
+  //     return map
+  // }
+  getNextId = id => {
+    console.log(id)
+    const nextChanged = this.state.data.find(elem => elem.id === id)
+    console.log(nextChanged.questions.length)
 
-    render() {
-        console.log(this.state)
-        return(
-            <div>
-                <h3>{this.state.page.text}</h3>
-                {this.renderButton()}
-            </div>
-        )
-
-    }   
+    nextChanged.questions.length === 0
+      ? console.log("is true") ||
+        this.setState({ page: nextChanged, isLoad: false })
+      : console.log("is false") ||
+        this.setState({ page: nextChanged, isLoad: true })
+  }
+  render() {
+    console.log(this.state)
+    const { data, page, isLoad } = this.state
+    return (
+      <div>
+        <h3>{page.text}</h3>
+        {isLoad
+          ? console.log("mamamama") ||
+            page.questions.map(question => (
+              <input
+                type="submit"
+                value={question.value}
+                onClick={() => this.getNextId(question.nextId)}
+              />
+            ))
+          : console.log("mimimimimim") || <div>ffff</div>}
+      </div>
+    )
+  }
 }
 
 export default Text
