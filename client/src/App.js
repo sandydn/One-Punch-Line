8598b4d25b3c158cf0b2c5ca0f8835732278914a
Sandy Das Neves <sandydn78@gmail.com>
2019-10-26 02:13:40 +0200

    003-router | delete the links on App

--- client/src/App.js
+++ client/src/App.js
@@ -6,20 +6,7 @@ import {
 import Index from './routes/Index'
 function App() {
   return (
-    <Index>
-      <div className="App">
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/fight">Fight</Link>
-              </li>
-            </ul>
-          </nav>
-      </div>
-    </Index>
+    <Index></Index>
   );
 }
 
