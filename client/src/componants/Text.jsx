066a8a1b1fb175a311579a28c367aa638360b4d7
QuietMonkey <taillandierbertrand@gmail.com>
2019-10-26 07:14:28 +0200

    005-create-client | feat(Script): refacto script & add endpoint logic

--- client/src/componants/Text.jsx
+++ client/src/componants/Text.jsx
@@ -1,53 +1,41 @@
 import React, { Component } from "react"
-// import Start from '../data/Start.json'
+import Button from '../charter/Button'
 
-class Text extends Component {
+export default class TextScript extends Component<Props> {
   state = {
     data: this.props.data,
     page: this.props.data[0],
     isLoad: true
   }
 
-  // setPage = async(id) => {
-  //     const poulet = await this.props.data.filter((one)=> one.id.includes(id))
-  //     console.log(poulet)
-  //     await this.setState({page: poulet})
-  // }
-
-  // renderButton = () =>{
-  //     const map = !!this.state.nextId ? console.log('REDIRECT') : this.state.page.questions.map((one) => <button onClick={() => this.setPage(one.nextId)}>{one.value}</button>)
-  //     return map
-  // }
   getNextId = id => {
-    console.log(id)
     const nextChanged = this.state.data.find(elem => elem.id === id)
-    console.log(nextChanged.questions.length)
 
     nextChanged.questions.length === 0
-      ? console.log("is true") ||
-        this.setState({ page: nextChanged, isLoad: false })
-      : console.log("is false") ||
-        this.setState({ page: nextChanged, isLoad: true })
+      ? this.setState({ page: nextChanged, isLoad: false })
+      : this.setState({ page: nextChanged, isLoad: true })
   }
+
   render() {
-    console.log(this.state)
-    const { data, page, isLoad } = this.state
+    const { data, 
+            page: { text, goFight, questions }, 
+            isLoad } 
+          = this.state
+
     return (
       <div>
-        <h3>{page.text}</h3>
+        <h3>{text}</h3>
         {isLoad
-          ? console.log("mamamama") ||
-            page.questions.map(question => (
-              <input
-                type="submit"
-                value={question.value}
-                onClick={() => this.getNextId(question.nextId)}
-              />
-            ))
-          : console.log("mimimimimim") || <div>ffff</div>}
+          ? questions.map(question => (
+            <input
+              type="submit"
+              value={question.value}
+              onClick={() => this.getNextId(question.nextId)}
+            />
+          ))
+          : <Button value={'FIGHT !'} path={goFight} isRedirect/>}
       </div>
     )
   }
 }
 
-export default Text
--- client/src/routes/HomePage.jsx
+++ client/src/routes/HomePage.jsx
@@ -1,19 +1,16 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom'
 import Button from '../charter/Button'
-import Text from '../componants/Text'
+import TextScript from '../componants/Text'
 import Start from '../data/Start.json'
 
 const HomePage = () => {
 
-    const startGame = (value) => <NavLink to="/fight" >{value}</NavLink>
-
-
     return (
         <div>
             <h1>One Punch Line</h1>
-            <Button handleClick={startGame} value={'New Game'}/>
-            <Text data={Start}/>
+            {/* <Button value={'New Game'} path={"/fight"} isRedirect/> */}
+            <TextScript data={Start}/>
         </div>
     )
 } 
